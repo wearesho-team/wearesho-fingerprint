d5c9fa2100863d83d5c294a82eb2aeb6aecdacfd
Beton <aleksa.yagich@gmail.com>
2018-07-18 17:06:11 +0300

    Refactor generator`s structure, perform saving timestamp fingerprint to cookie

--- README.md
+++ README.md
@@ -4,17 +4,16 @@
 
 ### Setup
 
-Create instance of generator, set default fingerprint components and [logger](./src/LoggerInterface.ts) (optional):
+Create instance of generator, set function that generates default fingerprint components:
 
 ```typescript
 import { FingerprintGenerator } from "@wearesho/fingerprint-generator";
-import Raven from "raven-js";
 
-const generator = new FingerprintGenerator({
+const generator = new FingerprintGenerator(() => ({
     resolution: "1600,900",
     timezone_offset: new Date().getTimezoneOffset(),
     anyOtherComponentKey: "anyOtherComponentValue",
-}, Raven);
+}));
 ```
 
 ### Generate unique fingerprint
--- src/FingerprintGenerator.ts
+++ src/FingerprintGenerator.ts
@@ -1,9 +1,10 @@
 import Fingerprint2 from "fingerprintjs2";
-import { LoggerInterface } from "./LoggerInterface";
+import Cookies from "js-cookie";
 
 export interface FingerprintComponents {
     resolution: string;
     timezone_offset: number;
+
     [K: string]: any;
 }
 
@@ -13,47 +14,56 @@ export interface FingerprintData {
 }
 
 export class FingerprintGenerator {
-    protected logger?: LoggerInterface;
-    protected defaultComponents: FingerprintComponents;
+    public static readonly timestampCookieKey = "bobra.timestamp-fingerprint";
 
-    protected get defaultToken(): string {
-        return (
-            Date.now()
-            + Math.random().toString().replace(/\./g, "")
-            + Math.random().toString().replace(/\./g, "")
-        ).substr(0, 32);
-    }
+    protected generateDefaultComponents: () => FingerprintComponents;
 
-    public constructor(defaultComponents: object = {}, logger?: LoggerInterface) {
-        this.logger = logger;
-
-        this.defaultComponents = {
+    protected get defaultComponents(): FingerprintComponents {
+        return {
             resolution: `${window.outerWidth},${window.outerHeight}`,
             timezone_offset: new Date().getTimezoneOffset(),
-            ...defaultComponents,
-        };
+            ...this.generateDefaultComponents(),
+        }
+    }
+
+    public constructor(generateDefaultComponents: () => FingerprintComponents) {
+        this.generateDefaultComponents = generateDefaultComponents;
     }
 
     public generate(): Promise<FingerprintData> {
-        return new Promise((resolve: (data: FingerprintData) => void) => {
-            try {
-                new Fingerprint2().get((token, components) => {
-                    resolve({
-                        token,
-                        components: {
-                            ...this.defaultComponents,
-                            ...components,
-                        },
-                    });
-                });
-            } catch (error) {
-                this.logger && this.logger.captureException(error);
+        return this.generateFP2()
+            .catch((error) => {
+                console.error(error);
+                return this.generateTimestamp();
+            });
+    }
 
-                resolve({
-                    token: this.defaultToken,
-                    components: this.defaultComponents,
-                });
-            }
+    protected generateFP2(): Promise<FingerprintData> {
+        return new Promise((resolve: (data: FingerprintData) => void) => {
+            new Fingerprint2().get((token, components) => resolve({
+                token,
+                components: {
+                    ...this.defaultComponents,
+                    ...components,
+                },
+            }));
         });
+    }
+
+    protected generateTimestamp(): FingerprintData {
+        const cookieToken = Cookies.get(FingerprintGenerator.timestampCookieKey);
+
+        const targetValue = {
+            token: cookieToken || (
+                Date.now()
+                + Math.random().toString().replace(/\./g, "")
+                + Math.random().toString().replace(/\./g, "")
+            ).substr(0, 32),
+            components: this.defaultComponents,
+        };
+
+        Cookies.set(FingerprintGenerator.timestampCookieKey, targetValue.token);
+
+        return targetValue;
     }
 }
